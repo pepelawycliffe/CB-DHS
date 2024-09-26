6f9e7af874a24652d877b7358fc533d7e00c7fca
wycliffe <wycliffepepela01@gmail.com>
2024-09-26 08:01:14 +0300

    first commit
    Signed-off-by: wycliffe <wycliffepepela01@gmail.com>

--- app/src/main/java/com/example/cb_dhs/utils/Constants.kt
+++ app/src/main/java/com/example/cb_dhs/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.cb_dhs.utils
 
 object Constants {
+    //    https://hiedhs.intellisoftkenya.com/auth/provider/register
     val BASE_URL = "https://ecommerce-deployed.onrender.com/"
 
 }
